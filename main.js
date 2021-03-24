479c76f35e1eb52d732119053c352178e8060810
jparrott06 <jparrott06@hotmail.com>
2021-03-24 15:11:59 -0600

    MongoDB Classwork added on top of prior Classwork

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require("express"), app = express(),
 homeController = require("./controllers/homeController"),
 errorController = require("./controllers/errorController"),
-layouts = require("express-ejs-layouts");
+layouts = require("express-ejs-layouts"), mongoose = require("mongoose"),
+subcribersController = require("./controllers/subscribersController");
+
+mongoose.connect("mongodb://localhost:27017/confetti_cuisine", {useNewUrlParser: true});
 
 app.set("port", process.env.PORT || 3000);
 
@@ -21,8 +24,12 @@ app.use(
 app.use(express.json());
 
 app.get("/courses", homeController.showCourses);
-app.get("/contact", homeController.showSignUp);
-app.post("/contact", homeController.postedSignUpForm);
+app.get("/subscribers", subcribersController.getAllSubscribers);
+app.get("/contact", subcribersController.getSubscriptionPage);
+app.post("/subscribe", subcribersController.saveSubscriber);
+
+// app.get("/contact", homeController.showSignUp);
+// app.post("/contact", homeController.postedSignUpForm);
 
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
